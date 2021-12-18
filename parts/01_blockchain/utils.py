b11d69503dd5b2c56c1c7e34f478cb926a3fb5b7
acarril <acarril@princeton.edu>
2021-12-18 15:37:52 -0500

    accept bytes or strings in compute_hash() method

--- parts/01_blockchain/utils.py
+++ parts/01_blockchain/utils.py
@@ -1,4 +1,9 @@
 from hashlib import sha256
+from typing import Union
 
-def compute_hash(data: bytes) -> str:
+def compute_hash(data: Union[bytes, str]) -> str:
+    try:
+        data = data.encode()   # https://stackoverflow.com/a/34870210
+    except AttributeError:
+        pass
     return sha256(data).hexdigest()
